918ef975684fda26b2e80cfed75ec448ffb8bee2
fernandopda <fernandopdassis@gmail.com>
2023-11-21 06:47:19 +1100

    - Intro-icons

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,17 +1,18 @@
 import introSoccerBalls from "./img/dalle_bestgoat.png";
+import { IconSoccer, IconTv, IconVote } from "./img/Icons";
 
 function Intro({ }) {
     return (
         <div className="intro-container">
             <div className="intro-boxes">
                 <div className="intro-box">
-
+                    <IconSoccer />
                 </div>
                 <div className="intro-box">
-
+                    <IconVote />
                 </div>
                 <div className="intro-box">
-
+                    <IconTv />
                 </div>
             </div>
             <div className="intro-balls-image">
